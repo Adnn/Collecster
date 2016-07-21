18f959a003b6d1144cba2361ad1bf7f948e8d574
Adnn <Adnn@users.noreply.github.com>
2016-07-22 00:19:19 +0430

    Fixes collecster exec so it uses absolute path to the filename, to avoid problems with working directory.

--- data_manager/collecster_exec.py
+++ data_manager/collecster_exec.py
@@ -1,9 +1,11 @@
 from inspect import currentframe
+from django.conf import settings
+
 import os
 
 def collecster_exec(filename):
     caller_frame = currentframe().f_back
     caller_frame.f_globals["__loader__"] = None
-    with open(filename) as f:
+    with open(os.path.join(settings.BASE_DIR, filename)) as f:
         code = compile(f.read(), os.path.abspath(f.name), 'exec')
         exec(code, caller_frame.f_globals, caller_frame.f_locals)
