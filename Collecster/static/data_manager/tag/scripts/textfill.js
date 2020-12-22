776c29ded5cd3b15ccd68b11c4d40e9b99c46b17
Adrien <Adnn@users.noreply.github.com>
2020-12-22 09:36:53 +0100

    Reworks tag v2 layout, more robust textfill.js implementation

--- Collecster/static/data_manager/tag/scripts/textfill.js
+++ Collecster/static/data_manager/tag/scripts/textfill.js
@@ -1,59 +1,65 @@
 ;(function($) {
 $.fn.textfill = function(options) {
-    //var fontSize = options.maxFontPixels;
+    // CSS font-size will act as the upper-limit, since it is the starting value for recursion
     var fontSize = parseInt($(this).css("font-size"));
     var ourText = $('.tweak:visible:first', this);
+    // We rely on the flow giving correct sizes to the element
+    // (even when the text overflows due to initial fontsize)
     var maxHeight = $(this).height();
     var maxWidth = $(this).width();
-    var cssMaxWidth = ourText.css('max-width');
 
+    // Note: I am not sure this is useful anymore.
+    // the parent of the .tweak element might specify a max-width
+    // ensure it is taken into consideration
     parent = ourText.parent();
     cssMaxWidth = parent.css("max-width");
     if (cssMaxWidth !== 'none')
-    { 
+    {
         maxWidth = parseInt(cssMaxWidth);
         //ourText.css("max-width", "none"); // error, then the text is not limited anymore, so it does not wrap...
     }
 
     var textHeight;
     var textWidth;
+    // Recurse to find the maximal font-size fitting in maxWidth,maxHeight
     do {
-        //ourText.css('font-size', fontSize);
         $(this).css('font-size', fontSize);
-        textHeight = ourText.outerHeight(true);
-        textWidth = ourText.outerWidth(true);
-        fontSize = fontSize - 1;
+        // Attention: ourText.outerHeight() returns the decimal height as a rounded int
+        // This can break the layout when rounding to a lower int
+        textWidth = Math.ceil(ourText[0].getBoundingClientRect().width);
+        textHeight = Math.ceil(ourText[0].getBoundingClientRect().height);
+        fontSize = fontSize - 1; // Used by next iteration
     } while ((textHeight > maxHeight || textWidth > maxWidth) && fontSize > 3);
 
-    $(this).height(textHeight); //resizes the ".jtextfill" to tightly hug its ".tweak" content
+    // Resizes the ".jtextfill" to tightly hug its ".tweak" content
+    // This allows to keep relative distance between release name and platform,
+    // even if release is made smaller.
+    $(this).height(textHeight);
 
-    //if (cssMaxWidth !== "none")
-    //{
-    //    ourText.css("max-width", textWidth);
-    //}
+    // If the parent specified a max-width, it is better to resize it so it hugs the text content
+    // (otherwise it might use max-width as it width, wasting horizontal space that could be used by other elements)
     if (cssMaxWidth !== "none")
     {
         parent.css("max-width", textWidth)
     }
 
-    // If the release_name freed up some space, give it the toe platform div
-    if ($(this).hasClass("release_name")) 
-    {
-        $(".platform").height($(".upper_content").outerHeight() - ($(".sp1").outerHeight()+textHeight))
-    }
+    // Note: commented-out, we keep the platform height static now
+    //// If the release_name freed up some space, give it to the platform div
+    //if ($(this).hasClass("release_name"))
+    //{
+    //    $(".platform").height($(".upper_content").height()
+    //                            - $(".sp1").outerHeight()
+    //                            - $(this).outerHeight())
+    //}
 
     return this;
 }
 })(jQuery);
 
 var fun = function() {
-//$(window).load(function() {
-    //$('.jtextfill').textfill({ maxFontPixels: 200 });
     $('.jtextfill').each(function(index)
                          {
                              $(this).textfill();
                          });
 }
-//$(document).ready(fun);
 $(window).load(fun);
-$(document).fun = fun;
